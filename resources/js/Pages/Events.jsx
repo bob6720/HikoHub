b86d4de01d40062870d21d8e0d6c9a8b93bacf8c
em0096 <mitchellevie96@gmail.com>
2025-09-11 15:34:42 +1200

    implement pagination

--- resources/js/Pages/Events.jsx
+++ resources/js/Pages/Events.jsx
@@ -1,48 +1,59 @@
 import React, { useState, useMemo } from 'react';
 import Layout from '@/Layouts/MainLayout';
 import EventCard from '@/Components/EventCard';
+import { Link } from '@inertiajs/react'; // not react-router-dom
 
 export default function Events({ events }) {
   // check for events
-  if (events.length == 0){
+  if (events.data.length == 0){
     return (
       <div className="text-center mt-10 text-gray-400">
         No upcoming events found.
       </div>
     )
   }
-  // useMemo stores the sorted list; it only recalculates when `events` changes
-  const sortedEvents = useMemo(() => {
-  // Copy events from parent and sort by event_date
-    return [...events].sort((a, b) => {
-        return new Date(a.event_date) - new Date(b.event_date);
-    });
 
-  }, [events]); // dependencies 
+  const isFirstPage = !events.prev_page_url; 
 
   return (
-    <Layout>
+    <Layout> {/* Main Layout Component*/}
       <div className="max-w-5xl mx-auto px-4 py-8">
 
-        <div>
+        <div> {/* Page title */}
           <h1 className="text-3xl font-bold mb-6 text-center text-purple-700">Events</h1>
         </div>
 
         {/* Search bar */}
-        <div>
-
-        </div>
+        <div></div>
 
         {/* Events grid */}
         {/* / Fit as many columns as possible, otherwise expand to fill space. */} 
         <div className="grid gap-6" style={{gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))'}}>
           {
-
-            sortedEvents.map(event => (
+            events.data.map(event => (
               // build card for each event
               <EventCard key={event.id} event = {event}/>
             ))
           }
+        </div>
+
+        {/* Pagination controls */}
+        <div className="flex items-center justify-center gap-4 mt-8">
+          
+          {/* Prev: only show if not first page */}
+          {!isFirstPage && (
+            <Link href={events.prev_page_url} className="px-4 py-2 rounded border border-white-700 hover:bg-pink-600">
+              Previous
+            </Link>  
+          )}
+
+          {/* Next page */}
+          {events.next_page_url && (
+            <Link href={events.next_page_url} className="px-4 py-2 rounded border border-white-700 hover:bg-pink-600">
+            Load More..
+            </Link>
+          )}
+
         </div>
 
       </div>
