310c7676ad677bf79c923be177f63e1f8d7d2959
em0096 <mitchellevie96@gmail.com>
2025-09-15 07:44:41 +1200

    update search bar

--- resources/js/Components/EventCard.jsx
+++ resources/js/Components/EventCard.jsx
@@ -3,8 +3,8 @@ import { calendarLink } from "@/Utils/calendarLink";
 export default function EventCard({event}){
     return(
         <div className="bg-black rounded shadow-md p-6 border border-gray-800 hover:shadow-lg transition-shadow duration-300">
-            <a href={calendarLink(event)} target="_blank" rel="noopener noreferrer" className="mt-3 inline-block rounded-md bg-blue-600 px-3 py-2 text-white text-sm hover:bg-blue-700"> 
-                Add to Calendar {/* Add icon vector */}
+            <a href={calendarLink(event)} target="_blank" rel="noopener noreferrer" className="mt-3 inline-block rounded-md bg-purple-600 px-3 py-2 text-white text-sm hover:bg-blue-700"> 
+                Add to Calendar {/* Add calendar icon vector */}
             </a>
             
             <h2 className="text-xl font-semibold mb-2 text-purple-800">{event.event_name}</h2>
