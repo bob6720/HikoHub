0b73650a3519eb9b10f8b4861c92e77cf165ce30
em0096 <mitchellevie96@gmail.com>
2025-10-13 12:51:12 +1300

    Change post-login redirect from public events page to /events-list

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -50,7 +50,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/');
+        return redirect()->intended('/events-list');
     }
 
     /**
